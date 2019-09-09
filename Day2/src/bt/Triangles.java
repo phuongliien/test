223506bd34aefa61619dfa3c640b3ac2e3de9a45
Phương Liên <47776275+phuongliien@users.noreply.github.com>
2019-09-09 17:34:57 +0700

    change 2

--- Day2/src/bt/Triangles.java
+++ Day2/src/bt/Triangles.java
@@ -5,12 +5,58 @@
  */
 package bt;
 
+import java.util.Scanner;
+
 /**
  *
  * @author DELL
  */
 public class Triangles {
+
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        float a = s.nextFloat();
+        System.out.println("Enter b: ");
+        float b = s.nextFloat();
+        System.out.println("Enter c: ");
+        float c = s.nextFloat();
+        int result = checkTriangle(a, b, c);
+        if (result < 0) {
+            System.out.println("this is not a triangle");
+        } else {
+            float p = (a + b + c) / 2;
+            double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+            switch (result) {
+                case 1:
+                    System.out.println("this is deu tam giac");
+                    break;
+                case 2:
+                    System.out.println("this is can tam giac");
+                    break;
+                default :
+                    System.out.println("this is v tam giac");
+                    break;
+                    
+            }
+            System.out.println("P=" + (p * 2));
+            System.out.println("S= "+area);
+        }
+    }
+
+    public static int checkTriangle(float a, float b, float c) {
+        if ((a + b > c) && (a + c > b) && (b + c > a)) {
+            if ((a == b) && (a == c)) {
+                return 1;
+            }
+            if ((a == b) || (a == c) || (b == c)) {
+                return 2;
+            }
+            if ((a * a + b * b == c * c)) {
+                return 3;
+            }
+            return 0;
+        }
+        return -1;
     }
 }
